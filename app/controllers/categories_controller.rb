d14867d7e735e58750d5cae2bd289712ec191a71
samiullah <bahadursamiullah@gmail.com>
2023-01-11 15:48:35 +0430

    Add destroy method in category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,6 +19,13 @@ class CategoriesController < ApplicationController
 
     end 
 
+    def destroy 
+        @category=Category.find(params[:id])
+        @category.destroy
+        redirect_to categories_path
+    end 
+
+
     private 
 
     def category_params 
