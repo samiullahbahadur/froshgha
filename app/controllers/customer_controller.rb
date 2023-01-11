8a1dea38507e5dd1e270ecfac986c8ac7a123df9
samiullah <bahadursamiullah@gmail.com>
2023-01-11 14:58:08 +0430

    add all controllers

---
+++ app/controllers/customer_controller.rb
@@ -0,0 +1,32 @@
+class CustomerController < ApplicationController
+  def index
+    @customer= Customer.all
+  end
+  def new
+    @customer = Customer.new
+  end
+
+   def create 
+
+    @customer= Customer.new(customer_params)
+     if @customer.save
+      redirect_to customer_index_path
+     else
+      render 'new'
+     end
+    end
+
+
+    def destroy
+      @customer = Customer.find(params[:id])
+      @customer.destroy
+      redirect_to customer_index_path
+  end
+
+  private 
+
+ def customer_params
+  params.require(:customer).permit(:full_name, :address, :phone)
+ end 
+
+end
