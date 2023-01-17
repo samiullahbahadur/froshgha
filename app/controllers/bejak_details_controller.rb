5276b842c72500dcf7da2a362a5a9bba84b3d1b4
samiullah <bahadursamiullah@gmail.com>
2023-01-17 23:28:37 +0430

    add all controller and actions

---
+++ app/controllers/bejak_details_controller.rb
@@ -0,0 +1,56 @@
+class BejakDetailsController < ApplicationController
+
+  before_action  :set_bejak_details, only: [:edit, :update, :destroy]
+  def index
+    @bejak_details = BejakDetail.all
+  end
+
+
+  def edit 
+
+  end 
+
+   def update 
+    if @bejak_details.update(bejak_details_params)
+      redirect_to bejak_details_path
+    else
+      render "edit"
+    end
+   end 
+
+
+  def new 
+     @bejak_details=BejakDetail.new
+  end
+
+  def create 
+     @bejak=Bejak.last
+    @bejak_details=BejakDetail.new(bejak_details_params)
+     @bejak_details.bejak= @bejak
+    if  @bejak_details.save
+      
+      redirect_to bejak_details_path
+    else 
+      render "new"
+    end
+  end 
+   
+ 
+ 
+
+  
+def destroy
+  @bejak_details.destroy
+  redirect_to bejak_details_path
+end
+
+  private 
+
+  def bejak_details_params 
+    params.permit(:details, :unit, :quantity, :unit_price, :product_id)
+  end 
+
+  def set_bejak_details
+    @bejak_details=BejakDetail.find(params[:id])
+  end
+end
