8ae05bd7d90b047dc8bc2ac0d9bcb91a4fbd8b46
samiullah <bahadursamiullah@gmail.com>
2023-01-23 12:16:44 +0430

    update application controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_172830) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_184124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,6 +103,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_17_172830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "fullname"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "wastes", force: :cascade do |t|
