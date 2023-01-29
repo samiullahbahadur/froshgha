de0a9ece3d99c5e711712b29c4ed6a88a34d06ad
samiullah <bahadursamiullah@gmail.com>
2023-01-29 15:44:12 +0430

    add search features

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,16 @@
 class Customer < ApplicationRecord
    has_many  :invoices,  dependent: :destroy
+   paginates_per 2
+
+  
+
+   self.inheritance_column="not_sti"
+   def self.search(search)
+      if search
+         where(["full_name LIKE ? ", "%#{search}%"])
+      else   
+        all.order(created_at: :desc)
+      end 
+
+   end
 end
