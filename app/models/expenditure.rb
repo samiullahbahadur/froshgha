ed5b8505707638b2a2c8a2000466880a63237b84
samiullah <bahadursamiullah@gmail.com>
2023-01-30 23:11:04 +0430

    update expenditure and invoice details models

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,2 +1,16 @@
 class Expenditure < ApplicationRecord
+
+    
+  paginates_per 4
+
+  self.inheritance_column="not_sti"
+  def self.search(search)
+     if search
+        where([" CAST( date AS  text) LIKE ? ", "%#{search}%"])   
+      
+     else   
+       all.order(created_at: :desc)
+     end 
+
+  end
 end
