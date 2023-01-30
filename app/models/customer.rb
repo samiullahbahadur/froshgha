d8b37ff5323d77fde32f13f3817a22c0ad692d72
samiullah <bahadursamiullah@gmail.com>
2023-01-30 23:10:28 +0430

    update current price and customer models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,10 @@
 class Customer < ApplicationRecord
    has_many  :invoices,  dependent: :destroy
-   paginates_per 2
+   paginates_per 5
 
-  
 
    self.inheritance_column="not_sti"
+   
    def self.search(search)
       if search
          where(["full_name LIKE ? ", "%#{search}%"])
